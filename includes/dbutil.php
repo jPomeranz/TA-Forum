a4f9a50212e080d9f707c2fa775f2e987bc59550
Jacob Pomeranz <jrpomeranz@yahoo.com>
2017-04-17 10:09:22 -0400

    Revert "Added review functionality. TA+section ids come from GET passed from browse. Fixed spacing throughout."
    This reverts commit 2f2431d2e03d48f12e70cbccc2f952594cb3ccdc.

--- includes/dbutil.php
+++ includes/dbutil.php
@@ -1,26 +1,26 @@
 <?php
 class DbUtil{
-    public static $loginUser = "***REMOVED***"; 
-    public static $loginPass = "***REMOVED***$";
-    public static $loginUser_insecure = "***REMOVED***"; 
-    public static $loginPass_insecure = "***REMOVED***";
-    public static $host = "***REMOVED***"; // DB Host
-    public static $schema = "***REMOVED***"; // DB Schema
-    
-    public static function loginConnection($secure = true){
-        $db = null;
-        if($secure == true)
-            $db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
-        else
-            $db = new mysqli(DbUtil::$host, DbUtil::$loginUser_insecure, DbUtil::$loginPass_insecure, DbUtil::$schema);
-        if($db->connect_errno){
-            echo("Could not connect to db");
-            $db->close();
-            exit();
-        }
-        
-        return $db;
-    }
+	public static $loginUser = "***REMOVED***"; 
+	public static $loginPass = "***REMOVED***$";
+	public static $loginUser_insecure = "***REMOVED***"; 
+	public static $loginPass_insecure = "***REMOVED***";
+	public static $host = "***REMOVED***"; // DB Host
+	public static $schema = "***REMOVED***"; // DB Schema
+	
+	public static function loginConnection($secure = true){
+		$db = null;
+		if($secure == true)
+			$db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
+		else
+			$db = new mysqli(DbUtil::$host, DbUtil::$loginUser_insecure, DbUtil::$loginPass_insecure, DbUtil::$schema);
+		if($db->connect_errno){
+			echo("Could not connect to db");
+			$db->close();
+			exit();
+		}
+		
+		return $db;
+	}
 
 }
 ?>
--- index.php
+++ index.php
@@ -1,36 +1,36 @@
 <head>
-    <?php
+	<?php
         include "includes/head.php";
     ?>
 </head>
 
 <body>
-    <?php
-        $currentPage = "index";
-        include "includes/navbar.php";
-    ?>
+	<?php
+		$currentPage = "index";
+		include "includes/navbar.php";
+	?>
 
-    <div class="container">
-        <?php
-            require "includes/dbutil.php";
+	<div class="container">
+		<?php
+			require "includes/dbutil.php";
             require "includes/userFuncs.php";
 
-            $db = DbUtil::loginConnection(true);
+			$db = DbUtil::loginConnection(true);
 
-            $stmt = $db->stmt_init();
+			$stmt = $db->stmt_init();
 
-            echo "<h2>Browse Courses By School:</h2>";
-            $result = mysqli_query($db, "SELECT DISTINCT school FROM dept");
-            while($row = mysqli_fetch_array($result)) {
-                echo "<p><b>" . $row['school'] . ":</b></p><div class='check-margin2'>";
-                $subresult = mysqli_query($db, "SELECT course_dept FROM dept WHERE school=\"" . $row['school'] . "\" ORDER BY school");
-                while($subrow = mysqli_fetch_array($subresult)) {
-                    echo $subrow['course_dept'] . "<br>";
-                }
-                echo "</div>";
-            }
+			echo "<h2>Browse Courses By School:</h2>";
+			$result = mysqli_query($db, "SELECT DISTINCT school FROM dept");
+			while($row = mysqli_fetch_array($result)) {
+				echo "<p><b>" . $row['school'] . ":</b></p><div class='check-margin2'>";
+				$subresult = mysqli_query($db, "SELECT course_dept FROM dept WHERE school=\"" . $row['school'] . "\" ORDER BY school");
+				while($subrow = mysqli_fetch_array($subresult)) {
+					echo $subrow['course_dept'] . "<br>";
+				}
+				echo "</div>";
+			}
 
-            $db->close();
-        ?>
+			$db->close();
+		?>
     </div>
 </body>
