d4918060813514ae36ee3eef78ddc17c6b8d29a4
Jacob Pomeranz <jrpomeranz@yahoo.com>
2017-04-21 10:57:16 -0400

    Add info to export
    Add a few more columns to the export data.

--- export.php
+++ export.php
@@ -9,9 +9,9 @@
     $stmt = $db->stmt_init();
 
     $results_array = array();
-    $result = mysqli_query($db, "SELECT course_dept, course_mnemonic_number, section_number, description, name FROM dept NATURAL JOIN course NATURAL JOIN section_of NATURAL JOIN section NATURAL JOIN review_about NATURAL JOIN review NATURAL JOIN ta");
+    $result = mysqli_query($db, "SELECT course_dept, course_mnemonic_number, section_number, semester, year, description, timestamp, name, graduation_year FROM dept NATURAL JOIN course NATURAL JOIN section_of NATURAL JOIN section NATURAL JOIN review_about NATURAL JOIN review NATURAL JOIN ta");
     while($row = mysqli_fetch_array($result)) {
-        $results_array[] = array('Course Dept'=>$row['course_dept'], 'Course Mnemonic Number'=>$row['course_mnemonic_number'], 'Section Number'=>$row['section_number'], 'Review Description'=>$row['description'], 'TA Name'=>$row['name']);
+        $results_array[] = array('Course Dept'=>$row['course_dept'], 'Course Mnemonic Number'=>$row['course_mnemonic_number'], 'Section Number'=>$row['section_number'], 'Semester'=>$row['semester'], 'Year'=>$row['year'], 'Review Description'=>$row['description'], 'Review Timestamp'=>$row['timestamp'], 'TA Name'=>$row['name'], 'TA Graduation Year'=>$row['graduation_year']);
     }
 
     $db->close();
